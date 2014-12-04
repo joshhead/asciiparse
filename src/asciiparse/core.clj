5fb930752d98789913c2301d029731ab8158958c
Josh Headapohl <joshhead@gmail.com>
2014-12-03 23:30:08 -0500

    Parse well-formed input

--- src/asciiparse/core.clj
+++ src/asciiparse/core.clj
@@ -1,7 +1,49 @@
 (ns asciiparse.core
+  (:require [clojure.string :as string])
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def coll-to-n
+  {[\space \_ \space \| \space \| \| \_ \|]                 0
+   [\space \space \space \space \space \| \space \space \|] 1
+   [\space \_ \space \space \_ \| \| \_ \space]             2
+   [\space \_ \space \space \_ \| \space \_ \|]             3
+   [\space \space \space \| \_ \| \space \space \|]         4
+   [\space \_ \space \| \_ \space \space \_ \|]             5
+   [\space \_ \space \| \_ \space \| \_ \|]                 6
+   [\space \_ \space \space \space \| \space \space \|]     7
+   [\space \_ \space \| \_ \| \| \_ \|]                     8
+   [\space \_ \space \| \_ \| \space \_ \|]                 9})
+
+(defn transpose-entry-chars
+  "Given a sequence of 3 strings with 27 characters each, return a
+  sequence of 9 sequences of 9 characters each.  First seq will be
+  Line 1 characters 1, 2, 3, line 2 characters 1, 2, 3, line 3
+  characters 1, 2, 3.  Second seq will be Line 1 characters 4, 5, 6,
+  line 2 characters 4, 5, 6, line 3 characters 4, 5, 6. And so on."
+  [input]
+  (->> input
+       (map #(partition 3 %))
+       (apply #(map list %1 %2 %3))
+       (map flatten)))
+
+(defn three-of-four-seq
+  "Given a collection, return a seq of three entries at a time,
+  dropping every fourth entry."
+  [lines]
+  (->> lines
+       (partition 4)
+       (map #(take 3 %))))
+
+(defn reduce-entry
+  "Take one entry as three ascii-art lines and convert it to a seq of
+  numbers, appending the result to the accumulated collection of
+  entries."
+  [entries three-lines]
+  (conj entries (map coll-to-n (transpose-entry-chars three-lines))))
+
+(defn transform-lines
+  "Given a sequence of input ascii-art lines, return a sequence of
+  converted numbers"
+  [lines]
+  (reduce reduce-entry [] (three-of-four-seq lines)))
--- test/asciiparse/core_test.clj
+++ test/asciiparse/core_test.clj
@@ -1,7 +1,20 @@
 (ns asciiparse.core-test
   (:require [clojure.test :refer :all]
-            [asciiparse.core :refer :all]))
+            [asciiparse.core :refer :all]
+            [clojure.java.io :as io]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parse-well-formed-file
+  (testing "Read in well formed input and get expected output."
+    (let [parsed (with-open [reader (io/reader "testdata/wellformed.txt")]
+                   (doall (transform-lines (line-seq reader))))]
+      (is (= parsed [[1 2 3 4 5 6 7 8 9]
+                     [0 0 0 0 0 0 0 0 0]
+                     [1 1 1 1 1 1 1 1 1]
+                     [2 2 2 2 2 2 2 2 2]
+                     [3 3 3 3 3 3 3 3 3]
+                     [4 4 4 4 4 4 4 4 4]
+                     [5 5 5 5 5 5 5 5 5]
+                     [6 6 6 6 6 6 6 6 6]
+                     [7 7 7 7 7 7 7 7 7]
+                     [8 8 8 8 8 8 8 8 8]
+                     [9 9 9 9 9 9 9 9 9]])))))
